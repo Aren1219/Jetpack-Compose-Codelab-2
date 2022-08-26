513f2378a27432906f54743cad6606a6d1e7356d
Aren1219 <arenwang1219@gmail>
2022-08-26 14:14:32 +0100

    first commit

---
+++ app/src/main/java/com/example/jetpackcomposestatecodelab/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.jetpackcomposestatecodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.jetpackcomposestatecodelab.ui.theme.JetpackComposeStateCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeStateCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+fun Preview(){
+    JetpackComposeStateCodelabTheme {
+        WellnessScreen()
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = {count ++}, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier){
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+){
+    Column (modifier){
+        StatefulCounter()
+        WellnessTaskList(
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            list = wellnessViewModel.tasks,
+            onCloseTask = {task -> wellnessViewModel.remove(task)},
+        )
+    }
+
+}
---
+++ app/src/main/java/com/example/jetpackcomposestatecodelab/WellnessTaskList.kt
@@ -0,0 +1,35 @@
+package com.example.jetpackcomposestatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.jetpackcomposestatecodelab.ui.WellnessTaskItem
+
+//private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit
+){
+    LazyColumn(
+        modifier = modifier,
+    ){
+        items(
+            items = list,
+            key = {task -> task.id}
+        ){ task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = {onCloseTask(task)},
+                checked = task.checked,
+                onCheckChange = { checked -> onCheckedTask(task,checked) }
+            )
+        }
+    }
+}
